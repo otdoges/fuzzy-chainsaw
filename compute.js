1e8b40e0ddcecad1b87386f991ae40686f599de1
otdoges <otdoges@proton.me>
2025-03-07 00:18:04 -0600

    Added Fastly setup

---
+++ compute.js
@@ -0,0 +1,12 @@
+/// <reference types="@fastly/js-compute" />
+import { Router } from '@fastly/expressly';
+import { NextServer } from '@fastly/next';
+
+const app = new Router();
+const next = new NextServer();
+
+app.use(async (req, res) => {
+  await next.handle(req, res);
+});
+
+app.listen();
